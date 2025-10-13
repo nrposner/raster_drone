482bbda8f20864fbe608d38f6a8516b51f94649b
Nicolas R Posner <nicolasposner@gmail.com>
2025-10-13 14:39:40 -0500

    updating process_image docstring to include types

--- src/raster.rs
+++ src/raster.rs
@@ -74,13 +74,20 @@ impl FromPyObject<'_> for SamplingType {
 /// Processes a black and white image into a sample of coordinate pixels
 ///
 /// Arguments:
-///     input_path: path to source image
-///     n: number of pixels
-///     threshold: brightness threshold that gets counted as a 'white' pixel
-///     sample: selecting type of sampling, either 'grid' or 'farthest'. Defaults to 'farthest'
-///     img_type: selecting type of image, either 'black_on_white' or 'white_on_black'. Defaults to 'black_on_white'
-///     resize: resizing 
-///     output_path: path where the output coordinates image will be saved. Note that, if the intermediate directories do not exist, they will be created. Defaults to 'output/coordinates.png'
+///     input_path: str 
+///         path to source image
+///     n: u32
+///         number of pixels to select
+///     threshold: f64 
+///         brightness threshold that gets counted as a 'white' pixel
+///     sample: str
+///         selecting type of sampling, either 'grid' or 'farthest'. Defaults to 'farthest'
+///     img_type: str 
+///         selecting type of image, either 'black_on_white' or 'white_on_black'. Defaults to 'black_on_white'
+///     resize: (width: u32, height: u32)
+///         maximum dimensions by which to resize the image. Will not be resized to exactly those dimensions, but instead to fit within them. Defaults to width = 256, height = 256. Set to None to prevent resizing
+///     output_path: str
+///         path where the output coordinates image will be saved. Note that, if the intermediate directories do not exist, they will be created. Defaults to 'output/coordinates.png'
 ///
 pub fn process_image(
     input_path: String, 
